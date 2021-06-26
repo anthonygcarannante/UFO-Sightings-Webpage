6239cba559301ed420960e885e54c0b7fd27763e
carannantea <79670978+carannantea@users.noreply.github.com>
2021-06-26 14:32:49 -0400

    Updated Level 1 code for added robustness

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -1,43 +1,49 @@
 // from data.js
 var tableData = data;
 
+// Declare tbody as variable
+let ufoTable = d3.select("tbody");
+
 // Select the filter button
 var button = d3.select("#filter-btn");
 
-// Select the form
-var form = d3.select("#form");
-
 // Create event handlers for clicking the button
-button.on("click", filterDate);
-form.on("submit", filterDate);
+button.on("click", handleClick);
 
-function filterDate() {
+function createTable(data) {
+  // Clear existing table
+  ufoTable.html("");
 
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
+  // Create function with d3 to update each cell's text with all values found from the filtered data
+  // [datetime, city, state, country, shape, duration, comments]
+  filterData.forEach(function(dataRow) {
+  console.log(dataRow);
+  let row = ufoTable.append("tr");
 
-    // Select the input element to get the raw HTML node
-    var inputElement = d3.select("#dateInput");
-
-    // Select the value from the datetime element
-    var inputDate = inputElement.property("value");
-    console.log(`UFO Sightings on: ${inputDate}`);
-
-    // Get the data from the date inputted by the user
-    var filteredData = tableData.filter(date => date.datetime == inputDate);
-
-    // Create function with d3 to update each cell's text with all values found from the filtered data
-    // [datetime, city, state, country, shape, duration, comments]
-    filteredData.forEach(function(insertData) {
-        console.log(insertData);
-        var ufoTable = d3.select("#ufo-table");
-        var row = ufoTable.append("tr");
-
-        // Append a cell into the row with each value in the ufo table object
-        Object.entries(insertData).forEach(function([key,value]) {
-            var cell = row.append("td");
-            cell.text(value);
-        });
+  // Append a cell into the row with each value in the ufo table object
+  Object.entries(dataRow).forEach(([key,value]) => {
+    let cell = row.append("td");
+    cell.text(value);
     });
-
+  });
 };
+
+function handleClick() {
+  // Prevents page from refreshing
+  d3.event.preventDefault();
+
+  // Select the value from the datetime element
+  let inputDate = d3.select("#datetime").property("value");
+  console.log(`UFO Sightings on: ${inputDate}`);
+
+  // Get the data from the date inputted by the user
+  if(inputDate) {
+  filterData = tableData.filter(date => date.datetime == inputDate);
+  };
+
+  // Create table with filtered data from the date input
+  createTable(filterData);
+};
+
+// Create table on default webpage from data source.
+createTable(tableData);
